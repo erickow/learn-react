a6c04813ec481d90bcd8935796627569de4b66be
Ericko Wicaksono <ericko.wicaksono@gmail.com>
2019-04-15 16:03:13 +0700

    initial section 2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ import ReactDOM from "react-dom";
 
 // Create a react component
 const App = () => {
-  return <div>Hi there! </div>;
+  const buttonText = { text: "Click me!" };
+  const labelText = "Enter name : ";
+  const style = {
+    backgroundColor: "blur",
+    color: "white"
+  };
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={style}>{buttonText.text}</button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
