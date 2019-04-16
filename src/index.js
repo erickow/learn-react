75c2c17d3571b1c7ab33225488e3180ccb016050
Ericko Wicaksono <ericko.wicaksono@gmail.com>
2019-04-16 11:25:35 +0700

    Comunication with props and parent children component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,38 @@
 // Import the Reat and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 // Create a react component
 const App = () => {
-  const buttonText = { text: "Click me!" };
-  const labelText = "Enter name : ";
-  const style = {
-    backgroundColor: "blue",
-    color: "white"
-  };
-
   return (
-    <div>
-      <label className="label" htmlFor="name">
-        {labelText}
-      </label>
-      <input id="name" type="text" />
-      <button style={style}>{buttonText.text}</button>
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 19:00"
+          content="Halo There ?"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Boy"
+          timeAgo="Yesterday at 09:00"
+          content="Who are You"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Rose"
+          timeAgo="Today at 12:00"
+          content="Nice to meet you"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
