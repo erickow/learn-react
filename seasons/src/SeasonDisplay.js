5af8d94d159a2b9d87935c85315891a5b5180a49
Ericko Wicaksono <ericko.wicaksono@gmail.com>
2019-04-26 11:06:20 +0700

    learn class lifecycle, state, conditional rendering, css

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,37 @@
+import "./SeasonDisplay.css";
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>Season display template</div>;
+const seasonConfig = {
+  summer: {
+    iconName: "sun",
+    text: "Let's hit the beach"
+  },
+  winter: {
+    iconName: "snowflake",
+    text: "Burr, it is chilly"
+  }
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+
+  const { iconName, text } = seasonConfig[season];
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
