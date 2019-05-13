219b3cca7fe66ffb030e1ae1671cd71572ea73f1
Ericko Wicaksono <ericko.wicaksono@gmail.com>
2019-05-13 21:04:52 +0700

    redux store design

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,9 +1,29 @@
 import jsonPlaceHolder from "../api/jsonPlaceHolder";
+// import _ from "lodash";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  console.log(getState().posts);
+};
 
 export const fetchPosts = () => async dispatch => {
-  const promise = await jsonPlaceHolder.get("/posts");
+  const response = await jsonPlaceHolder.get("/posts");
   dispatch({
     type: "FETCH_POSTS",
-    payload: promise
+    payload: response.data
   });
 };
+
+export const fetchUser = id => async (id, dispatch) => {
+  const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
